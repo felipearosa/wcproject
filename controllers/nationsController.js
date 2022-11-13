3d06958f86beb71ab5468b438fa0d83741f68c29
Felipe <felipe.arosa@gmail.com>
2022-11-13 20:31:20 -0300

    created route and function for getting nations

--- controllers/nationsController.js
+++ controllers/nationsController.js
@@ -1,4 +1,5 @@
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 const Nation = require('./../models/nationModel');
 
 exports.getAllNations = catchAsync(async (req, res) => {
@@ -8,5 +9,20 @@ exports.getAllNations = catchAsync(async (req, res) => {
     status: 'sucess',
     results: nations.length,
     nations
+  })
+})
+
+exports.getNation = catchAsync(async (req,res) => {
+  const nation = await Nation.findById(req.params.id);
+
+  if(!nation){
+    return next(new AppError('The ID is wrong!', 404))
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      nation
+    }
   })
 })
